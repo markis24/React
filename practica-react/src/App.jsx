ba590578c74fa3f7c7a8c4f6fbc4b9203cc5caa5
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-04-04 14:50:30 +0200

    Practica React

---
+++ practica-react/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Header from './components/nav/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
